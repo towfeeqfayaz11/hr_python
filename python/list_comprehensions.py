0019840d53a78d57921b4b78d43d3af26eeaf42c
Fayaz, Towfeeq <towfeeq.fayaz@rakuten.com>
2021-03-09 23:52:06 +0900

    9-mar

--- python/list_comprehensions.py
+++ python/list_comprehensions.py
@@ -2,10 +2,17 @@
 
 
 #solution(1):
-x, y, z, n = int(input()), int(input()), int(input()), int(input())
+x, y, z, n = int(input()), int(input()), int(input()), int(input())        # for taking input from n lines
 print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
 
 
 #solution(2):
-x, y, z, n = (int(input()) for _ in range(4))
+x, y, z, n = (int(input()) for _ in range(4))    # for taking inout from n lines
 print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
+
+
+
+NOTE: 
+x, y, z, n = int(input()), int(input()), int(input()), int(input())    # this will take input from 4 lines as assign accordingly to variables
+x, y, z, n = (int(input()) for _ in range(4)) # this will take input from 4 lines as assign accordingly to variables
+x, y, z, n = mao(int, input().split()) # this will take input from single line (will assign 4 inputs to variable on left side accordingly)
---
+++ python/print_1_to_n_as_string.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    print(range(1, int(input())+1), sep='')
+
+
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end='')
